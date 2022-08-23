136e33d600dad011351e2d9dacefe5030c3bb8b4
Ilias Karimalis <iliaskarimalis@outlook.com>
2022-08-23 00:17:56 -0700

    Implemented Rule of 5 for Bitset
    Added custom destructor, copy/move ctor, copy/move assignment operator
    for Bitset; additionally added various pieces of functionality that were
    needed for the core optimization algorithm.
    
    Signed-off-by: Ilias Karimalis <iliaskarimalis@outlook.com>

--- gosdt/bitset.cpp
+++ gosdt/bitset.cpp
@@ -1,12 +1,16 @@
 #include "bitset.hpp"
 
+#include <iostream>
+
 namespace GOSDT {
 
-    Bitset::Bitset(::usize size, bool fill)
+    Bitset::Bitset()
+    : data(nullptr), n_blocks(0), size(0), n_bits_used_last_block(0)
+    {}
+
+    Bitset::Bitset(usize size, bool fill)
     : size(size)
     {
-        /* Allocate memory for the Bitset */
-
         // Find number of usize blocks that need to be allocated
         n_bits_used_last_block = size % BITS_PER_BLOCK;
         if (size == 0) {
@@ -14,11 +18,10 @@ namespace GOSDT {
             data = nullptr;
             return;
         } else {
-            n_blocks = (size / BITS_PER_BLOCK);
+            n_blocks = (long)(size / BITS_PER_BLOCK);
             // Add extra block that is lost to integer division above
             n_blocks += n_bits_used_last_block != 0;
         }
-
         // TODO For now we're using the default c++ allocator, but this might
         //      need to be changed in the future. Our default implementation
         //      used a tbb::scalable_allocator<usize>, it might make sense to
@@ -28,30 +31,89 @@ namespace GOSDT {
 
         // Fill the data with the specific value.
         if (fill) {
-
-            for (auto i = 0; i < n_blocks; ++i) {
+            for (auto i = 0; i < n_blocks; ++i)
+            {
                 data[i] = ~((usize) 0);
             }
 
-            // We need to set to 0 all values in the last byte which are unused
-            // which we do by right shifting the number of bits that we want to
-            // keep.
-            auto mask = ~((usize) 0) >>
-                (BITS_PER_BLOCK - n_bits_used_last_block);
+            auto mask = ~((usize) 0) >> (BITS_PER_BLOCK - n_bits_used_last_block);
             data[n_blocks - 1] &= mask;
 
         } else {
-            for (auto i = 0; i < n_blocks; ++i) {
+            for (auto i = 0; i < n_blocks; ++i)
+            {
                 data[i] = 0;
             }
         }
-
     }
 
-    std::optional<bool>
+    Bitset::Bitset(const Bitset& other)
+    : data(new usize[other.n_blocks]), n_blocks(other.n_blocks), size(other.size),
+    n_bits_used_last_block(other.n_bits_used_last_block)
+    {
+        for (mp_size_t i = 0; i < n_blocks; i++)
+        {
+            data[i] = other.data[i];
+        }
+    }
+
+    Bitset::Bitset(Bitset && other) noexcept
+    {
+        // Instantiate this
+        data = other.data;
+        n_blocks = other.n_blocks;
+        size = other.size;
+        n_bits_used_last_block = other.n_bits_used_last_block;
+
+        // Remove values from other
+        other.data = nullptr;
+        other.n_blocks = 0;
+        other.size = 0;
+        other.n_blocks = 0;
+    }
+
+    Bitset::~Bitset()
+    {
+        delete[] data;
+    }
+
+    Bitset&
+    Bitset::operator=(const Bitset &other)
+    {
+        if (this == &other) return *this;
+        delete[] data;
+        data = new usize[other.n_blocks];
+        n_blocks = other.n_blocks;
+        size = other.size;
+        n_bits_used_last_block = other.n_bits_used_last_block;
+        for (mp_size_t i = 0; i < n_blocks; i++)
+        {
+            data[i] = other.data[i];
+        }
+        return *this;
+    }
+
+    Bitset&
+    Bitset::operator=(Bitset &&other) noexcept
+    {
+        delete[] data;
+        data = other.data;
+        n_blocks = other.n_blocks;
+        size = other.size;
+        n_bits_used_last_block = other.n_bits_used_last_block;
+
+        other.data = nullptr;
+        other.n_blocks = 0;
+        other.size = 0;
+        other.n_bits_used_last_block = 0;
+        return *this;
+    }
+
+    bool
     Bitset::get(usize index) const
     {
-        if (size == 0 || index >= size || data == nullptr) return std::nullopt;
+        if (size == 0 || index >= size || data == nullptr)
+            throw std::out_of_range("Oh no!");
 
         auto block_index = index / BITS_PER_BLOCK;
         auto bit_index = index % BITS_PER_BLOCK;
@@ -81,6 +143,7 @@ namespace GOSDT {
     Bitset
     Bitset::bit_and(Bitset const& b1, Bitset const& b2, bool flip)
     {
+        std::cout << b1.size << " " << b2.size << std::endl;
         assert(b1.size == b2.size);
 
         auto ret_val = Bitset(b1.size, true);
@@ -99,13 +162,24 @@ namespace GOSDT {
         return mpn_zero_p(data, n_blocks);
     }
 
-    std::size_t
-    BitsetHash::operator()(const Bitset &bitset)
-    {
-        size_t hash = bitset.size;
-        return hash;
+    usize Bitset::scan(usize first, bool value) {
+        // TODO Bitset::scan
+        return 0;
     }
 
 
+    bool
+    operator==(const Bitset& left, const Bitset& right)
+    {
+        bool ret = left.n_blocks == right.n_blocks;
+        ret &= left.size == right.size;
+        ret &= left.n_bits_used_last_block == right.n_bits_used_last_block;
+        if (!ret) return ret;
+
+        for (mp_size_t i = 0; i < left.n_blocks; i++) {
+            ret &= left.data[i] == right.data[i];
+        }
+        return ret;
+    }
 
 }
--- gosdt/bitset.hpp
+++ gosdt/bitset.hpp
@@ -10,24 +10,15 @@
 namespace GOSDT {
 
     struct Bitset {
-
         // Not Portable. Requires that each 1 byte == 8 bits
         static const constexpr size_t BITS_PER_BLOCK = 8 * sizeof(usize);
 
-        // Internal memory structure
+        // pointer to bistet content
         usize * data = nullptr;
         // number of usize blocks allocated and stored in data
         mp_size_t n_blocks = 0;
-
-        // External memory structure
-        usize size; // number of booleans represented by this Bitset
-
-
-        // TODO I think that depth_budget should be encapsulated under Nodes
-        // not in the Bitset. The number of possible additional splits which
-        // can be conducted. If it's -1 that signifies that there is no depth
-        // budget
-
+        // number of booleans represented by this Bitset
+        usize size;
         // the number of bits used in the last used block
         u8 n_bits_used_last_block;
 
@@ -36,34 +27,53 @@ namespace GOSDT {
         //      bit_and where we know that we can avoid filling that Bitset.
 
         // Constructs a bitset from a single fill value and size
+        Bitset();
         Bitset(usize size, bool fill);
+        Bitset(const Bitset& other);
+        Bitset(Bitset&& other) noexcept;
+        ~Bitset();
 
-        [[nodiscard]] std::optional<bool>
+        Bitset& operator=(const Bitset& other);
+        Bitset& operator=(Bitset&& other) noexcept;
+
+        bool friend operator==(const Bitset& left, const Bitset& right);
+
+        [[nodiscard]] bool
         get(usize index) const;
 
         void
         set(usize index, bool value) const;
 
         // Assumes that the last block of memory is filled with zeros outside
-        // of the n_used_last_block bytes.
+        // the n_used_last_block bytes.
         [[nodiscard]] usize
         count() const;
-
-
-        // TODO add more functionality as we need it
 
         // flip: whether to treat the bits of b1 as flipped
         static Bitset
         bit_and(Bitset const& b1, Bitset const& b2, bool flip = false);
 
-        [[nodiscard]] bool empty() const;
+        [[nodiscard]] bool
+        empty() const;
 
+        usize
+        scan(usize first, bool value);
     };
 
+}
 
-    struct BitsetHash {
+namespace std {
 
-        std::size_t operator()(const Bitset &bitset);
+    template<>
+    struct hash<GOSDT::Bitset> {
+
+        size_t
+        operator()(const GOSDT::Bitset& bitset) const
+        {
+            // TODO: calculate the hash value of this bitset
+            return 0;
+        }
+
     };
 
 }
